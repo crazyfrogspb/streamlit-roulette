e849f92625ebb3a27a33f4c9963a766cccaebef4
Evgenii Nikitin <e.nikitin@nyu.edu>
2020-04-15 07:07:03 +0300

    Working version

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-import hashlib
-import io
-import os
-import random
 import time
 from typing import Dict
 
@@ -14,11 +10,10 @@ import numpy as np
 import streamlit as st
 from streamlit import caching
 
-from utils import get_session_id, select_block_container_style
+from utils import fancy_cache, select_block_container_style, SessionState
 
 
 # np.random.seed(24)
-user_session_id = get_session_id()
 
 
 def display_case(case):
@@ -29,8 +24,8 @@ def display_case(case):
     return case_md
 
 
-@st.cache(allow_output_mutation=True)
-def get_static_store(user_session_id) -> Dict:
+@fancy_cache(unique_to_session=True, allow_output_mutation=True)
+def get_static_store() -> Dict:
     """This dictionary is initialized once and can be used to store the files uploaded"""
     return {}
 
@@ -41,8 +36,10 @@ nltk.download("punkt")
 
 st.title("Рулетка кейсов")
 
+state = SessionState.get(cases=[], played_inds=[], orig_case_num=0, file_value=None)
+
 menu_state = st.radio("Показать меню", ["Показать", "Скрыть"], 0)
-static_store = get_static_store(user_session_id)
+static_store = get_static_store()
 
 file_picker = st.empty()
 file_buffer = file_picker.file_uploader("Загрузите файл с кейсами", type="txt")
@@ -55,94 +52,94 @@ else:
     static_store.clear()
 
 
-@st.cache(allow_output_mutation=True)
-def read_cases(file_value, user_session_id):
-    cases = static_store[file_value].splitlines()
-    cases = [case for case in cases if case]
-    orig_case_num = len(cases)
-    return cases, orig_case_num, []
+def read_cases(file_value, force=False):
+    if not state.cases or force or file_value != state.file_value:
+        cases = static_store[file_value].splitlines()
+        state.cases = [case for case in cases if case]
+        state.orig_case_num = len(state.cases)
+        state.played_inds = []
+        state.file_value = file_value
+
+
+def get_colors(orig_case_num, current_ind):
+    colors = ["lightgrey"] * orig_case_num
+    if state.played_inds:
+        for ind in state.played_inds:
+            colors[ind] = "dimgray"
+    if current_ind is not None:
+        colors[current_ind] = "indianred"
+
+    return colors
+
+
+def plot_wheel(played_inds=None, current_ind=None):
+    size = 360 / state.orig_case_num
+    sizes = [size] * state.orig_case_num
+    labels = [i for i in range(1, state.orig_case_num + 1)]
+    colors = get_colors(state.orig_case_num, current_ind)
+    fig1, ax1 = plt.subplots(figsize=(3, 3))
+    wedges, texts = ax1.pie(sizes, labels=labels, rotatelabels=True, startangle=0, colors=colors, counterclock=False)
+    for w in wedges:
+        w.set_linewidth(2)
+        w.set_edgecolor("white")
+    wheel.pyplot(fig1, clear_figure=False)
+    plt.close(fig1)
+
+    return wedges, fig1
 
 
 if file_buffer is not None:
-    cases, orig_case_num, played_inds = read_cases(file_buffer.getvalue(), user_session_id)
+    read_cases(file_buffer.getvalue())
 else:
-    cases = None
-    played_inds = []
-    orig_case_num = 0
+    state.cases = None
+    state.played_inds = []
+    state.orig_case_num = 0
     st.info("Загрузите хотя бы один файл")
 
 reload_button = st.empty()
 selection_button = st.button("Выбрать кейс")
-wheel = st.pyplot(label=user_session_id)
+wheel = st.empty()
 case_placeholder = st.empty()
-if reload_button.button("Перезагрузить файлы"):
-    caching.clear_cache()
-    static_store.clear()
-    played_inds = []
-    wheel.empty()
+if reload_button.button("Рестарт"):
+    if file_buffer:
+        read_cases(file_buffer.getvalue(), force=True)
+        wedges, fig1 = plot_wheel(played_inds=state.played_inds)
 
 if menu_state == "Скрыть":
     file_picker.empty()
     reload_button.empty()
 
 
-def get_colors(orig_case_num, current_ind):
-    colors = ["lightgrey"] * orig_case_num
-    if current_ind is not None:
-        colors[current_ind] = "indianred"
-    if played_inds:
-        for ind in played_inds:
-            colors[ind] = "dimgray"
-
-    return colors
-
-
-def plot_wheel(played_inds=None, current_ind=None):
-    size = 360 / orig_case_num
-    sizes = [size] * orig_case_num
-    labels = [i for i in range(1, orig_case_num + 1)]
-    colors = get_colors(orig_case_num, current_ind)
-    fig1, ax1 = plt.subplots()
-    wedges, texts = ax1.pie(sizes, labels=labels, rotatelabels=True, startangle=0, colors=colors, counterclock=False)
-    for w in wedges:
-        w.set_linewidth(2)
-        w.set_edgecolor("white")
-    while True:
-        try:
-            wheel.pyplot(fig1)
-            break
-        except IndexError:
-            pass
-
-    plt.close(fig1)
-
-    return wedges
-
-
-if cases:
-    wedges = plot_wheel(played_inds=played_inds)
-    if played_inds:
-        case_placeholder.markdown(display_case(cases[played_inds[-1]]))
+if state.cases:
+    wedges, fig1 = plot_wheel(played_inds=state.played_inds)
+    if state.played_inds:
+        case_placeholder.markdown(display_case(state.cases[state.played_inds[-1]]))
 
 if selection_button:
-    if len(played_inds) == orig_case_num:
+    if len(state.played_inds) == state.orig_case_num:
         case_placeholder.markdown("## Кейсы закончились!")
     else:
-        case_num = len(cases)
-        rand_choice = np.random.randint(orig_case_num * 3, orig_case_num * 5)
+        case_placeholder.empty()
+        case_num = len(state.cases)
+        rand_choice = np.random.randint(state.orig_case_num * 3, state.orig_case_num * 5)
         ind = 0
         for i in range(rand_choice):
-            while ind in played_inds:
-                ind += 1
-                if ind >= orig_case_num:
-                    ind = 0
-            if ind >= orig_case_num:
+            if ind >= state.orig_case_num:
                 ind = 0
-            plot_wheel(orig_case_num, ind)
-            time.sleep(i * 0.002)
+            while ind in state.played_inds:
+                ind += 1
+                if ind >= state.orig_case_num:
+                    ind = 0
+            colors = get_colors(state.orig_case_num, ind)
+            for w_num, w in enumerate(wedges):
+                w.set_color(colors[w_num])
+                w.set_linewidth(2)
+                w.set_edgecolor("white")
+            wheel.pyplot(fig1, clear_figure=False)
+            time.sleep(min(0.1, 0.005 + 0.0025 * i))
             ind += 1
-            if len(played_inds) == orig_case_num - 1:
+            if len(state.played_inds) == state.orig_case_num - 1:
                 break
-        played_inds.append(ind - 1)
-        case_placeholder.markdown(display_case(cases[ind - 1]))
-        st.markdown(f"### Осталось кейсов: {orig_case_num - len(played_inds)}")
+        state.played_inds.append(ind - 1)
+        case_placeholder.markdown(display_case(state.cases[ind - 1]))
+        st.markdown(f"### Осталось кейсов: {state.orig_case_num - len(state.played_inds)}")
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import functools
+import time
+
 import streamlit as st
 
 import streamlit.ReportThread as ReportThread
@@ -63,29 +66,153 @@ def get_session_id():
 
     ctx = ReportThread.get_report_ctx()
 
-    this_session = None
-
-    current_server = Server.get_current()
-    if hasattr(current_server, "_session_infos"):
-        # Streamlit < 0.56
-        session_infos = Server.get_current()._session_infos.values()
-    else:
-        session_infos = Server.get_current()._session_info_by_id.values()
+    session = None
+    session_infos = Server.get_current()._session_info_by_id.values()
 
     for session_info in session_infos:
         s = session_info.session
         if (
-            # Streamlit < 0.54.0
             (hasattr(s, "_main_dg") and s._main_dg == ctx.main_dg)
+            # Streamlit < 0.54.0
             or
             # Streamlit >= 0.54.0
             (not hasattr(s, "_main_dg") and s.enqueue == ctx.enqueue)
         ):
-            this_session = s
+            session = session_info.session
 
-    if this_session is None:
+    if session is None:
         raise RuntimeError(
             "Oh noes. Couldn't get your Streamlit Session object" "Are you doing something fancy with threads?"
         )
 
-    return id(this_session)
+    return id(session)
+
+
+def fancy_cache(func=None, ttl=None, unique_to_session=False, **cache_kwargs):
+    """A fancier cache decorator which allows items to expire after a certain time
+    as well as promises the cache values are unique to each session.
+    Parameters
+    ----------
+    func : Callable
+        If not None, the function to be cached.
+    ttl : Optional[int]
+        If not None, specifies the maximum number of seconds that this item will
+        remain in the cache.
+    unique_to_session : boolean
+        If so, then hash values are unique to that session. Otherwise, use the default
+        behavior which is to make the cache global across sessions.
+    **cache_kwargs
+        You can pass any other arguments which you might to @st.cache
+    """
+    # Support passing the params via function decorator, e.g.
+    # @fancy_cache(ttl=10)
+    if func is None:
+        return lambda f: fancy_cache(func=f, ttl=ttl, unique_to_session=unique_to_session, **cache_kwargs)
+
+    # This will behave like func by adds two dummy variables.
+    dummy_func = st.cache(
+        func=lambda ttl_token, session_token, *func_args, **func_kwargs: func(*func_args, **func_kwargs), **cache_kwargs
+    )
+
+    # This will behave like func but with fancy caching.
+    @functools.wraps(func)
+    def fancy_cached_func(*func_args, **func_kwargs):
+        # Create a token which changes every ttl seconds.
+        ttl_token = None
+        if ttl is not None:
+            ttl_token = int(time.time() / ttl)
+
+        # Create a token which is unique to each session.
+        session_token = None
+        if unique_to_session:
+            session_token = get_session_id()
+
+        # Call the dummy func
+        return dummy_func(ttl_token, session_token, *func_args, **func_kwargs)
+
+    return fancy_cached_func
+
+
+class SessionState(object):
+    def __init__(self, **kwargs):
+        """A new SessionState object.
+
+        Parameters
+        ----------
+        **kwargs : any
+            Default values for the session state.
+
+        Example
+        -------
+        >>> session_state = SessionState(user_name='', favorite_color='black')
+        >>> session_state.user_name = 'Mary'
+        ''
+        >>> session_state.favorite_color
+        'black'
+
+        """
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+
+    def get(**kwargs):
+        """Gets a SessionState object for the current session.
+
+        Creates a new object if necessary.
+
+        Parameters
+        ----------
+        **kwargs : any
+            Default values you want to add to the session state, if we're creating a
+            new one.
+
+        Example
+        -------
+        >>> session_state = get(user_name='', favorite_color='black')
+        >>> session_state.user_name
+        ''
+        >>> session_state.user_name = 'Mary'
+        >>> session_state.favorite_color
+        'black'
+
+        Since you set user_name above, next time your script runs this will be the
+        result:
+        >>> session_state = get(user_name='', favorite_color='black')
+        >>> session_state.user_name
+        'Mary'
+
+        """
+        # Hack to get the session object from Streamlit.
+
+        ctx = ReportThread.get_report_ctx()
+
+        this_session = None
+
+        current_server = Server.get_current()
+        if hasattr(current_server, "_session_infos"):
+            # Streamlit < 0.56
+            session_infos = Server.get_current()._session_infos.values()
+        else:
+            session_infos = Server.get_current()._session_info_by_id.values()
+
+        for session_info in session_infos:
+            s = session_info.session
+            if (
+                # Streamlit < 0.54.0
+                (hasattr(s, "_main_dg") and s._main_dg == ctx.main_dg)
+                or
+                # Streamlit >= 0.54.0
+                (not hasattr(s, "_main_dg") and s.enqueue == ctx.enqueue)
+            ):
+                this_session = s
+
+        if this_session is None:
+            raise RuntimeError(
+                "Oh noes. Couldn't get your Streamlit Session object" "Are you doing something fancy with threads?"
+            )
+
+        # Got the session object! Now let's attach some state into it.
+
+        if not hasattr(this_session, "_custom_session_state"):
+            this_session._custom_session_state = SessionState(**kwargs)
+
+        return this_session._custom_session_state
